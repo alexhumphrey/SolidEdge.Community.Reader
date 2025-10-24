81e3674326e2ab25c91cce3b48098cdb5a05e7d8
Alex Humphrey <alex.humphrey@gmail.com>
2025-10-24 08:15:37 +0100

    .net 9

--- src/SolidEdge.Community.Reader/Properties/AssemblyInfo.cs
+++ src/SolidEdge.Community.Reader/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SolidEdge.Community.Reader")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SolidEdge.Community.Reader")]
-[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,20 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4a024efd-4a69-4acb-8f4b-e3484c703401")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-// ONLY INCREMENT THIS VALUE ON MAJOR RELEASES!
-[assembly: AssemblyVersion("2.0.0.0")]
-
-// This version should match the NuGet version.
-[assembly: AssemblyFileVersion("2.0.0.0")]
